48c7709f30a0f79226cbbf8ec51813b8eefe0b45
Gnimacz <30383499+Gnimacz@users.noreply.github.com>
2022-09-22 15:22:29 +0200

    added a way to play single soundclips through code

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    [SerializeField] private AudioSource sound = new AudioSource();
+    [SerializeField] private AudioClip clip;
+    public void Sound()
+    {
+        sound.PlayOneShot(clip);
+    }
+}
